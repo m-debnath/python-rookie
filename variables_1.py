398011bbdcf7019d06729b904adc37ada0e84b9e
Mukul Debnath <Mukul Debnath>
2017-04-02 03:46:41 +0530

    Variables

---
+++ variables_1.py
@@ -0,0 +1,18 @@
+a=100
+print(a)
+
+b="mukul"
+print(b)
+
+print(b+str(a))
+
+def someFunction():
+#    global a
+    a="I am learning python"
+    print(a)
+
+someFunction()
+print(a)
+
+del(b)
+# print(b)
