80c8fb266e46feca2f11fe811bfd2294154437c3
Mukul Debnath <Mukul Debnath>
2017-04-02 06:44:17 +0530

    Fibonacci, Strings

---
+++ strings_1.py
@@ -0,0 +1,9 @@
+# This file is for learning strings
+var="Hello world!"
+print(var)
+
+# getting substring From to To-1
+var1="Mukul778"
+var2="Software Testing"
+print("var1[0]:",var1[0]);
+print("var2[1:9]:",var2[1:9])
